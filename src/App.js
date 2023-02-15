f3f247b7e983dcde41cba645db4348812e2a4900
hourglass2020 <pouria.fm15@gmail.com>
2023-02-15 20:07:42 +0330

    make project rtl & add fonts to it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import { Button } from '@mui/material';
-import './App.css';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import rtlPlugin from "stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import { HelmetProvider, Helmet } from "react-helmet-async";
+import createCache from "@emotion/cache";
+import { prefixer } from "stylis";
+
+import "./App.css";
+import { Button } from "@mui/material";
+
+const theme = createTheme({
+  direction: "rtl",
+  typography: {
+    fontFamily: 'Shabnam, Roboto'
+  }
+});
+
+const cacheRTL = createCache({
+  key: "muirtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
 
 function App() {
   return (
-    <div className="App">
-      <Button variant='contained'>Salam</Button>
-    </div>
+    <CacheProvider value={cacheRTL}>
+      <ThemeProvider theme={theme}>
+        <HelmetProvider>
+          <Helmet>
+            <title>وبسایت شخصی پوریا اقدم پور</title>
+          </Helmet>
+          <div className="App">
+            <Button variant="contained">کلیک کن</Button>
+          </div>
+        </HelmetProvider>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
