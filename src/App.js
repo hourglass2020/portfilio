9c763f5e1e850f479d93857cb9ce77d8e6688c74
hourglass2020 <pouria.fm15@gmail.com>
2023-02-19 20:00:18 +0330

    add imports

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
-import { ThemeProvider } from "@mui/material/styles";
-import rtlPlugin from "stylis-plugin-rtl";
-import { CacheProvider } from "@emotion/react";
-import { HelmetProvider, Helmet } from "react-helmet-async";
-import createCache from "@emotion/cache";
-import { prefixer } from "stylis";
-
-import "./App.css";
-
-import { theme } from "./components/ui/theme"
-import Header from "./components/ui/Header";
-
-const cacheRTL = createCache({
-  key: "muirtl",
-  stylisPlugins: [prefixer, rtlPlugin],
-});
+import MainLayout from "./components/layouts/MainLayout";
+import Header from './components/ui/Header';
 
 function App() {
   return (
-    <CacheProvider value={cacheRTL}>
-      <ThemeProvider theme={theme}>
-        <HelmetProvider>
-          <Helmet>
-            <title>وبسایت شخصی پوریا اقدم پور</title>
-          </Helmet>
-          <Header />
-        </HelmetProvider>
-      </ThemeProvider>
-    </CacheProvider>
+    <MainLayout>
+      <Header />
+    </MainLayout>
   );
 }
 
--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -1,4 +1,16 @@
-import React from 'react'
+import { ThemeProvider } from "@mui/material/styles";
+import rtlPlugin from "stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import { HelmetProvider, Helmet } from "react-helmet-async";
+import createCache from "@emotion/cache";
+import { prefixer } from "stylis";
+
+import { theme } from './../ui/theme';
+
+const cacheRTL = createCache({
+    key: "muirtl",
+    stylisPlugins: [prefixer, rtlPlugin],
+});
 
 function MainLayout({ children }) {
     return (
